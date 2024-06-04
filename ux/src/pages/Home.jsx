bc4d20494044847aa8ec7db3895b9bf8aa7ce85f
pikatostes <alejandro.rios.cyt@gmail.com>
2024-06-04 13:03:19 +0200

    Home Update

--- ux/src/pages/Home.jsx
+++ ux/src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import CarouselComponent from "../components/Carousel";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import Minifigures from "../assets/minifigures.webp";
 
 const Home = () => {
   const imageSizes = ["800x400", "800x600", "800x500"]; // Ejemplo de tamaños de imágenes
 
   return (
-    <>
-      <h1>Welcome to the Main Page</h1>
-      <CarouselComponent carouselSize="carousel-md" imageSizes={imageSizes} />
-    </>
+    <Container
+      fluid
+      className="d-flex flex-column align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <h1>Welcome to BrickPoint!</h1>
+      {/* <CarouselComponent carouselSize="carousel-md" imageSizes={imageSizes} /> */}
+      <Row className="align-items-center">
+        <Col>
+          <Image src={Minifigures} alt="Minifigures" fluid />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
