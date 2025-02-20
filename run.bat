c8b8d956df8a01d3cc15cd73616cda5ea5ec565c
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2025-02-20 20:29:49 +0100

    lan

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ rem Función para ejecutar symfony serve en la carpeta api
 echo Ejecutando symfony serve en la carpeta api...
 cd test-api || exit /b
 symfony server:stop
-start /B symfony server:start --bind=0.0.0.0 --port=8000
+start /B symfony server:start --port=8000
 
 rem Función para ejecutar npm run dev en la carpeta ux
 :run_npm_dev
--- ux/vite.config.js
+++ ux/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
   }
 })
